b68b5e790d5b3de22eedc475d4fb8f5740bb4785
Tobi <Administrator@DESKTOPVBNET>
2023-03-02 14:21:28 +0100

    Anlegen des neuen ToDoItemControllers, des dazugehoerigen Interfaces und Services

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -1,8 +1,13 @@
 using WebApp;
 using WebApp.Interfaces;
 using WebApp.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using WebApp.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<WebAppContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("WebAppContext") ?? throw new InvalidOperationException("Connection string 'WebAppContext' not found.")));
 
 // Add services to the container.
 
@@ -12,6 +17,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddTransient<IWeatherService, WeatherServiceImpl>();
 builder.Services.AddSingleton<ISummaryService, SummaryServiceImpl>();
+builder.Services.AddSingleton<IToDoService, ToDoServiceImpl>();
 
 var app = builder.Build();
 
---
+++ WebApp/Services/ToDoServiceImpl.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.AccessControl;
+using WebApp.Data;
+using WebApp.Interfaces;
+using WebApp.Models;
+
+namespace WebApp.Services
+{
+    public class ToDoServiceImpl : IToDoService
+    {
+        TodoItem[] IToDoService.getTodo()
+        {
+            throw new NotImplementedException();
+        }
+
+        TodoItem IToDoService.getTodo(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IToDoService.insert(TodoItem todo)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IToDoService.delete(TodoItem todo)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IToDoService.update(TodoItem todo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
